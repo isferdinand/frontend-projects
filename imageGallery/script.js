6ba62d9262f1ce30c58b24e710724147f9aebc66
isferdinand <ikipkogey@kabarak.ac.ke>
2023-11-13 17:33:10 +0300

    Image sllder initial layout

---
+++ imageGallery/script.js
@@ -0,0 +1,14 @@
+const images = document.querySelectorAll('.image');
+
+images.forEach((image) => {
+  image.addEventListener('click', () => {
+    removeClass();
+    image.classList.add('active');
+  });
+});
+
+function removeClass() {
+  images.forEach((image) => {
+    image.classList.remove('active');
+  });
+}
