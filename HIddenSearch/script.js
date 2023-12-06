76335a1d124d5d247df08bd5a86e2575f33e8fa4
isferdinand <ikipkogey@kabarak.ac.ke>
2023-12-06 22:05:23 +0300

    Deleted

--- HIddenSearch/script.js
+++
@@ -1,8 +0,0 @@
-const btn = document.querySelector('.btn');
-const searchDiv = document.querySelector('.search');
-const input = document.querySelector('.search-bar');
-
-btn.addEventListener('click', () => {
-  searchDiv.classList.toggle('active');
-  input.focus();
-});
